63794e16dab494cc6f5ef65b19e05a4f39b90d1c
Swapna Devi <65804603+Swapna804@users.noreply.github.com>
2021-04-12 21:18:30 +0530

    added code

---
+++ Basics/takeInput.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scn=new Scanner(System.in);
+	    int n= Integer.parseInt(scn.nextLine());
+	    String name=scn.nextLine();
+	    System.out.println("dear"+name+"here is the counting");
+	    for(int i=1;i<n;i++){
+	        System.out.println(i);
+	    }
+		System.out.println("Hello World");
+	}
+}
